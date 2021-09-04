d2575e5c170a4fd91d31ae480b19a3400c7d2e8c
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-09-05 02:15:02 +0530

    Created template

---
+++ template/game.js
@@ -0,0 +1,29 @@
+function update() {
+
+}
+
+function clicked() {
+
+}
+
+function moved() {
+
+}
+
+function released() {
+
+}
+
+function render() {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, canvas_width, canvas_height);
+}
+
+function updateParams(variable) {
+
+}
+
+function initParams() {
+
+}
+
